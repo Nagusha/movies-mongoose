409992981bff0b358b337ae8432aa0fc4644f414
Nagusha <160119980+Nagusha@users.noreply.github.com>
2024-10-03 23:44:21 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 
 async function connection() {
     await mongoose.connect('mongodb://localhost:27017/nagusha', {
-        //useNewUrlParser: true,
-        //useUnifiedTopology: true, 
+       
     });
     console.log('Successfully connected to the database');
 }
